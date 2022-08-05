7ec2ca5ade55036d184a700677d9af02971c885c
MrSabin <sabin.21011986@gmail.com>
2022-08-05 14:32:28 +0300

    Got salary info for python developer vacancies

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_vacancies(role):
+def get_vacancies_count(role):
     base_api_url = "https://api.hh.ru/vacancies"
     payload = {"HH-User-Agent": "dvmn_salary", "area": "1", "text": role}
     response = requests.get(base_api_url, params=payload)
@@ -9,11 +9,26 @@ def get_vacancies(role):
     return total_vacancies
 
 
-languages = ["JavaScript", "Java", "Python", "Ruby", "PHP", "C++", "C#", "C", "Go"]
-search_result = {}
-for language in languages:
-    role = "Программист {}".format(language)
-    vacancies_count = get_vacancies(role)
-    search_result[language] = vacancies_count
-print(search_result)
+def get_vacancies(role):
+    base_api_url = "https://api.hh.ru/vacancies"
+    payload = {"HH-User-Agent": "dvmn_salary", "area": "1", "text": role}
+    response = requests.get(base_api_url, params=payload)
+    vacancies = response.json()
+    return vacancies
 
+
+def vacancy_by_language():
+    languages = ["JavaScript", "Java", "Python", "Ruby", "PHP", "C++", "C#", "C", "Go"]
+    search_result = {}
+    for language in languages:
+        role = "Программист {}".format(language)
+        vacancies_count = get_vacancies_count(role)
+        search_result[language] = vacancies_count
+    print(search_result)
+
+
+vacancy_role = "Программист Python"
+filtered_vacancies = get_vacancies(vacancy_role)
+
+for vacancy in filtered_vacancies["items"]:
+    print(vacancy["salary"])
