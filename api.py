4422d7b19f20698361c5f50cb7b6a25edca5e27d
MrSabin <sabin.21011986@gmail.com>
2022-08-05 13:07:53 +0300

    Got vacancies count by programming language

--- api.py
+++ api.py
@@ -1,12 +1,19 @@
 import requests
 
-base_api_url = "https://api.hh.ru/vacancies"
-payload = {"HH-User-Agent": "dvmn_salary", "professional_role": "96", "area": "1"}
-response = requests.get(base_api_url, params=payload)
-total_vacancies = response.json().get("found")
-print("Total developer vacancies in Moscow: {}".format(total_vacancies))
 
-payload["period"] = "30"
-response = requests.get(base_api_url, params=payload)
-total_vacancies = response.json().get("found")
-print("Total developer vacancies in Moscow last month: {}".format(total_vacancies))
+def get_vacancies(role):
+    base_api_url = "https://api.hh.ru/vacancies"
+    payload = {"HH-User-Agent": "dvmn_salary", "area": "1", "text": role}
+    response = requests.get(base_api_url, params=payload)
+    total_vacancies = response.json().get("found")
+    return total_vacancies
+
+
+languages = ["JavaScript", "Java", "Python", "Ruby", "PHP", "C++", "C#", "C", "Go"]
+search_result = {}
+for language in languages:
+    role = "Программист {}".format(language)
+    vacancies_count = get_vacancies(role)
+    search_result[language] = vacancies_count
+print(search_result)
+
