374b718c0ffa8d060b88b2cd404e564adb1b6634
MrSabin <sabin.21011986@gmail.com>
2022-08-05 10:44:46 +0300

    Received all developer vacancies in Moscow last month

--- api.py
+++ api.py
@@ -5,3 +5,8 @@ payload = {"HH-User-Agent": "dvmn_salary", "professional_role": "96", "area": "1
 response = requests.get(base_api_url, params=payload)
 total_vacancies = response.json().get("found")
 print("Total developer vacancies in Moscow: {}".format(total_vacancies))
+
+payload["period"] = "30"
+response = requests.get(base_api_url, params=payload)
+total_vacancies = response.json().get("found")
+print("Total developer vacancies in Moscow last month: {}".format(total_vacancies))
